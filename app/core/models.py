fdc3fefbf82e0c57d988bd8b72436ec61aed4208
VikramAnkathi <vikramankathi@gmail.com>
2023-07-07 23:52:57 -0400

    All flake changes are performed

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """ Manager for users """
 
@@ -17,7 +18,7 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError("User must have an email address,")
 
-        user = self.model(email= self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -27,10 +28,11 @@ class UserManager(BaseUserManager):
         """ create and return new superuser """
         user = self.create_user(email, password)
         user.is_staff = True
-        user.is_superuser  = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """  users in the system"""
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """ Tests for django admin """
 
@@ -14,14 +15,14 @@ class AdminSiteTests(TestCase):
         """ create user and client """
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@example.com',
-            password = 'testpass123',
+            email='admin@example.com',
+            password='testpass123',
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'user@example.com',
-            password = 'testpass123',
-            name = 'Test User',
+            email='user@example.com',
+            password='testpass123',
+            name='Test User',
         )
 
     def test_users_list(self):
@@ -42,6 +43,6 @@ class AdminSiteTests(TestCase):
     def test_create_user_page(self):
         """ Test the create user page works """
         url = reverse('admin:core_user_add')
-        res= self.client.get(url)
+        res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
